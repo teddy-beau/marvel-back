4f19878749bf1b7f7451326d6c23c8f254466f43
teddy-beau <hello@teddy-beau.com>
2021-02-18 12:24:02 +0100

    removed charcaters without picture

--- routes/characters.js
+++ routes/characters.js
@@ -24,7 +24,17 @@ router.get("/characters", async (req, res) => {
       const response = await axios.get(
          `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
       );
-      // console.log(response.data);
+      // Cleaning the API call results by removing charcaters without picture
+      for (let i = 0; i < response.data.results.length; i++) {
+         if (response.data.results[i].thumbnail) {
+            if (
+               response.data.results[i].thumbnail.path ===
+               "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+            ) {
+               response.data.results.splice(i, 1);
+            }
+         }
+      }
       res.status(200).json(response.data);
    } catch (error) {
       console.log(error);
